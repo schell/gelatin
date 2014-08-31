63f77c38b2ed40f66e772d5da152f40c09bdb7d4
Schell Scivally <efsubenovex@gmail.com>
2014-08-30 17:51:57 -0700

    uniform GADT

--- Gelatin/Rendering/Uniforms.hs
+++ Gelatin/Rendering/Uniforms.hs
@@ -1,39 +1,48 @@
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Gelatin.Rendering.Core where
 
 import Linear
 import Gelatin.Shaders.Core
 import Graphics.Rendering.OpenGL
+import Graphics.VinylGL
+import Graphics.GLUtil hiding (Elem, setUniform)
 import Control.Monad.Free
 import Control.Monad.Free.Church
-import Graphics.VinylGL
 import Data.Vinyl
 import Data.Vinyl.Universe
+import Data.Vinyl.Reflect
 
+data ShaderUniform where
+    ShaderUniform :: ( HasFieldNames (PlainFieldRec '[n ::: t])
+                     , HasFieldGLTypes (PlainFieldRec '[n ::: t])
+                     , SetUniformFields (PlainFieldRec '[n ::: t])
+                     ) => SField (n:::t) -> t -> ShaderUniform
 
-data SetUniform next where
-    SetUniform :: SField (n:::t) -> t -> next -> SetUniform next
+data SetUniform next = SetUniform ShaderUniform next
 
 type SetUniformCommand = F SetUniform
---
+
 instance Functor SetUniform where
-    fmap f (SetUniform u d next) = SetUniform u d $ f next
+    fmap f (SetUniform su next) = SetUniform su $ f next
 
-setUniform :: SField (n:::t) -> t -> SetUniformCommand ()
-setUniform n t = liftF $ SetUniform n t ()
+setUniform :: ( HasFieldNames (PlainFieldRec '[n ::: t])
+              , HasFieldGLTypes (PlainFieldRec '[n ::: t])
+              , SetUniformFields (PlainFieldRec '[n ::: t])
+              ) => SField (n:::t) -> t -> SetUniformCommand ()
+setUniform u d = liftF $ SetUniform (ShaderUniform u d) ()
 
+performUniformCommand :: ShaderProgram -> Free SetUniform () -> IO ()
 performUniformCommand _ (Pure ()) = return ()
-performUniformCommand s (Free (SetUniform loc u next)) = do
-    setUniforms s (loc =: u)
+performUniformCommand s (Free (SetUniform (ShaderUniform u m) next)) = do
+    setUniforms s (u =: m)
     performUniformCommand s next
 
 updatePJMV :: SetUniformCommand ()
 updatePJMV = do
-    setUniform projection (eye4 :: M44 GLfloat)
     setUniform modelview (eye4 :: M44 GLfloat)
+    setUniform projection (eye4 :: M44 GLfloat)
 
