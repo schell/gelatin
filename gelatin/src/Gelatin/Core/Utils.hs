a4d9341153080a1f36f60a763193f3cd679c44fd
Schell Scivally <efsubenovex@gmail.com>
2016-09-15 08:55:26 -0700

    unified transformation types and ops, simplified compilation model

---
+++ gelatin/src/Gelatin/Core/Utils.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Gelatin.Core.Utils where
+
+import Linear
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed (Vector, Unbox)
+
+-- | Determine if a point lies within a polygon path using the even/odd
+-- rule.
+-- A point is inside a path if it has an odd number of intersections with
+-- the boundary (Jordan Curve theorem)
+pathHasPoint :: (Ord a, Fractional a, Unbox a) => Vector (V2 a) -> V2 a -> Bool
+pathHasPoint vs v = V.foldr' (\s a -> if s then not a else a) False $
+  V.zipWith3 f vv vs (V.drop 1 vs)
+  where vv = V.replicate (V.length vs) v
+        f a b c = if t1 a b c && t2 a b c then True else False
+        t1 p p1 p2 = (y p2 > y p) /= (y p1 > y p)
+        t2 p p1 p2 = x p < (x p1 - x p2) * (y p - y p2) / (y p1 - y p2) + x p2
+        x (V2 a _) = a
+        y (V2 _ b) = b
