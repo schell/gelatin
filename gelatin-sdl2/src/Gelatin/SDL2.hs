ce217905086669a365e5d8fe941f0e9a1420d8d1
Schell Scivally <efsubenovex@gmail.com>
2017-10-27 16:02:28 -0700

    sdl2 docs

--- gelatin-sdl2/src/Gelatin/SDL2.hs
+++ gelatin-sdl2/src/Gelatin/SDL2.hs
@@ -2,40 +2,42 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
+-- | This module provides an entry point for your gelatin
+-- apps that run on sdl2.
 module Gelatin.SDL2
-  ( SDL2Backends(..)
+  ( -- * Backend definitions
+    SDL2Backends(..)
+    -- * Obtaining the backends
   , startupSDL2Backends
   , startupSDL2BackendsWithConfig
-  , module G
+    -- * Re-exports
+  , module Gelatin.GL
   ) where
 
 import           Control.Monad.Except   (MonadError)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.String            (fromString)
-import           Gelatin.GL             as G
+import           Gelatin.GL
 import           SDL                    hiding (Rectangle, Renderer,
                                          glBindTexture, glUnbindTexture)
 
+
+-- | A record containing both V2V4 and V2V2 backends.
 data SDL2Backends = SDL2Backends
   { backendV2V4 :: Backend GLuint Event V2V4 (V2 Float) Float Raster
   , backendV2V2 :: Backend GLuint Event V2V2 (V2 Float) Float Raster
   }
 
-startupSDL2Backends :: (MonadIO m, MonadError String m)
-                    => Int -> Int -> String -> Bool -> m SDL2Backends
-startupSDL2Backends ww wh ws highDPI = do
-    let openGL = defaultOpenGL{ glProfile = Core Debug 3 3
-                              }
-        window = defaultWindow{ windowInitialSize = V2 (fromIntegral ww)
-                                                       (fromIntegral wh)
-                              , windowOpenGL = Just openGL
-                              , windowResizable = True
-                              , windowHighDPI = highDPI
-                              }
-    startupSDL2BackendsWithConfig window ws
 
-startupSDL2BackendsWithConfig :: (MonadIO m, MonadError String m)
-                              => WindowConfig -> String -> m SDL2Backends
+-- | Start up and return the sdl2 backends according to the given
+-- sdl2 'WindowConfig'.
+startupSDL2BackendsWithConfig
+  :: (MonadIO m, MonadError String m)
+  => WindowConfig
+  -- ^ The configuration used to set up the window.
+  -> String
+  -- ^ The window title
+  -> m SDL2Backends
 startupSDL2BackendsWithConfig cfg str = do
   w <- liftIO $ do
     initializeAll
@@ -61,6 +63,34 @@ startupSDL2BackendsWithConfig cfg str = do
       v2v4 = Backend ops $ glV2V4Compiler rz
       v2v2 = Backend ops $ glV2V2Compiler rz
   return $ SDL2Backends v2v4 v2v2
+
+
+-- | Start up and return the default backends.
+-- Uses OpenGL 3.3 with debugging turned on.
+startupSDL2Backends
+  :: (MonadIO m, MonadError String m)
+  => Int
+  -- ^ Window width
+  -> Int
+  -- ^ Window height
+  -> String
+  -- ^ Window title
+  -> Bool
+  -- ^ Whether or not to request a high DPI window.
+  -- Passing 'True' typically results in a framebuffer with 2x
+  -- the window size.
+  -> m SDL2Backends
+startupSDL2Backends ww wh ws highDPI = do
+    let openGL = defaultOpenGL{ glProfile = Core Debug 3 3
+                              }
+        window = defaultWindow{ windowInitialSize = V2 (fromIntegral ww)
+                                                       (fromIntegral wh)
+                              , windowOpenGL = Just openGL
+                              , windowResizable = True
+                              , windowHighDPI = highDPI
+                              }
+    startupSDL2BackendsWithConfig window ws
+
 
 updateWindowSDL2 :: Window -> IO ()
 updateWindowSDL2 = glSwapWindow
